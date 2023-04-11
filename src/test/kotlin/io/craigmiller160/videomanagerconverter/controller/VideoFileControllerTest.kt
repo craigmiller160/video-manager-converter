d890dddfa73d8c3ff28806864327b80cd755ebf5
Craig Miller <craigmiller160@gmail.com>
2023-04-11 13:50:28 -0400

    More work on first test

--- src/test/kotlin/io/craigmiller160/videomanagerconverter/controller/VideoFileControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerconverter/controller/VideoFileControllerTest.kt
@@ -3,9 +3,12 @@ package io.craigmiller160.videomanagerconverter.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.craigmiller160.testcontainers.common.TestcontainersExtension
 import io.craigmiller160.testcontainers.common.core.AuthenticationHelper
+import io.craigmiller160.videomanagerconverter.domain.entity.ConvertStatus
 import io.craigmiller160.videomanagerconverter.domain.entity.FileToConvert
 import io.craigmiller160.videomanagerconverter.domain.repository.FileToConvertRepository
 import io.craigmiller160.videomanagerconverter.web.types.FileConversionRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,7 +19,9 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @SpringBootTest
 @ExtendWith(TestcontainersExtension::class, SpringExtension::class)
 @AutoConfigureMockMvc
@@ -44,6 +49,17 @@ class VideoFileControllerTest @Autowired constructor(
                     isNoContent()
                 }
             }
+
+        val results = repo.findAll()
+        assertEquals(1, results.size)
+
+        assertThat(results)
+            .hasSize(1)
+            .first()
+            .hasFieldOrPropertyWithValue("sourceFile", "/foo/bar/file.mkv")
+            .hasFieldOrPropertyWithValue("targetFile", "/foo/bar/file.mp4")
+            .hasFieldOrPropertyWithValue("status", ConvertStatus.PENDING)
+            .hasFieldOrPropertyWithValue("errorMessage", null)
     }
 
     @Test
