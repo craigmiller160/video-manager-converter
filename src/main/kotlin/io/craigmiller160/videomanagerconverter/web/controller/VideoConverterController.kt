57093d3fb835b69972d5f51ab53a4c83d4a1383c
Craig Miller <craigmiller160@gmail.com>
2023-04-11 12:21:58 -0400

    Got all endpoints implemented

--- src/main/kotlin/io/craigmiller160/videomanagerconverter/web/controller/VideoConverterController.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerconverter/web/controller/VideoConverterController.kt
@@ -1,7 +1,10 @@
 package io.craigmiller160.videomanagerconverter.web.controller
 
 import io.craigmiller160.videomanagerconverter.web.types.FileConversionRequest
+import io.craigmiller160.videomanagerconverter.web.types.FileConversionResponse
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,4 +20,15 @@ class VideoConverterController {
         TODO()
     }
 
+
+    @GetMapping
+    fun getConversionProgress(): List<FileConversionResponse> {
+        TODO()
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun clearPastConversions() {
+        TODO()
+    }
 }
