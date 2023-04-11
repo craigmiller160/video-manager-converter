056bea9c4f4aaa00d516dbf4396a8fe6f0e9bdce
Craig Miller <craigmiller160@gmail.com>
2023-04-11 15:08:36 -0400

    Added listeners

--- src/main/kotlin/io/craigmiller160/videomanagerconverter/service/ConversionOrchestrationService.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerconverter/service/ConversionOrchestrationService.kt
@@ -1,11 +1,23 @@
 package io.craigmiller160.videomanagerconverter.service
 
 import io.craigmiller160.videomanagerconverter.domain.repository.FileToConvertRepository
+import io.craigmiller160.videomanagerconverter.events.NewConversionEvent
+import jakarta.annotation.PostConstruct
+import org.springframework.context.event.ContextStartedEvent
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 
 @Service
 class ConversionOrchestrationService(
     private val fileToConvertRepository: FileToConvertRepository
 ) {
+    @EventListener
+    fun startup(event: ContextStartedEvent) {
+        TODO("Start for all pending")
+    }
 
+    @EventListener
+    fun newConversion(event: NewConversionEvent) {
+        TODO("New conversion")
+    }
 }
