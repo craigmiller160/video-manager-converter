f30ca17c95f6aee57f86c2f6799d6be7f6f5a20a
Craig Miller <craigmiller160@gmail.com>
2023-04-11 16:30:12 -0400

    Removed some terraform logic

--- deploy/terraform/roles.tf
+++ deploy/terraform/roles.tf
@@ -4,28 +4,6 @@ locals {
   }
 }
 
-data "keycloak_openid_client" "market_tracker_api_dev" {
-  realm_id  = data.keycloak_realm.apps_dev.id
-  client_id = "video-manager-server"
-}
-
-data "keycloak_openid_client" "market_tracker_api_prod" {
-  realm_id  = data.keycloak_realm.apps_prod.id
-  client_id = "video-manager-server"
-}
-
-data "keycloak_role" "market_tracker_api_access_role_dev" {
-  realm_id  = data.keycloak_realm.apps_dev.id
-  client_id = data.keycloak_openid_client.market_tracker_api_dev.id
-  name = local.access_role_common.name
-}
-
-data "keycloak_role" "market_tracker_api_access_role_prod" {
-  realm_id  = data.keycloak_realm.apps_prod.id
-  client_id = data.keycloak_openid_client.market_tracker_api_prod.id
-  name = local.access_role_common.name
-}
-
 resource "keycloak_role" "video_manager_converter_access_role_dev" {
   realm_id = data.keycloak_realm.apps_dev.id
   client_id = keycloak_openid_client.video_manager_converter_dev.id
@@ -36,23 +14,4 @@ resource "keycloak_role" "video_manager_converter_access_role_prod" {
   realm_id = data.keycloak_realm.apps_prod.id
   client_id = keycloak_openid_client.video_manager_converter_prod.id
   name = local.access_role_common.name
-}
-
-
-resource "keycloak_role" "video_manager_all_access_dev" {
-  realm_id = data.keycloak_realm.apps_dev.id
-  name = "video_manager_all_access"
-  composite_roles = [
-    data.keycloak_role.market_tracker_api_access_role_dev.id,
-    keycloak_role.video_manager_converter_access_role_dev.id
-  ]
-}
-
-resource "keycloak_role" "video_manager_all_access_prod" {
-  realm_id = data.keycloak_realm.apps_prod.id
-  name = "video_manager_all_access"
-  composite_roles = [
-    data.keycloak_role.market_tracker_api_access_role_prod.id,
-    keycloak_role.video_manager_converter_access_role_prod.id
-  ]
 }
