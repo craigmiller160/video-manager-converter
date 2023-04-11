14558bd228a3059116daf318dbca4e4dd6eb61d9
Craig Miller <craigmiller160@gmail.com>
2023-04-11 13:38:39 -0400

    Added testcontainers to config

--- src/test/kotlin/io/craigmiller160/videomanagerconverter/controller/VideoFileControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerconverter/controller/VideoFileControllerTest.kt
@@ -1,5 +1,6 @@
 package io.craigmiller160.videomanagerconverter.controller
 
+import io.craigmiller160.testcontainers.common.TestcontainersExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -7,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
-@ExtendWith(SpringExtension::class)
+@ExtendWith(TestcontainersExtension::class, SpringExtension::class)
 @AutoConfigureMockMvc
 class VideoFileControllerTest {
     @Test
