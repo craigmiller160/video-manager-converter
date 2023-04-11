6d908c94c5b073f2342883f465e63bbaa2a77c30
Craig Miller <craigmiller160@gmail.com>
2023-04-11 13:40:11 -0400

    More test setup logic

--- src/test/kotlin/io/craigmiller160/videomanagerconverter/controller/VideoFileControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/videomanagerconverter/controller/VideoFileControllerTest.kt
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
 @ExtendWith(TestcontainersExtension::class, SpringExtension::class)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class VideoFileControllerTest {
     @Test
     fun `starts a new conversion`() {
