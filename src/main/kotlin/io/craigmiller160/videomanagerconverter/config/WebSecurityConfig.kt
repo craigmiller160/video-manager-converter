330ba972b53bb23b5148e6523863d697d191b2a2
Craig Miller <craigmiller160@gmail.com>
2023-04-11 10:47:39 -0400

    More security config work

--- src/main/kotlin/io/craigmiller160/videomanagerconverter/config/WebSecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerconverter/config/WebSecurityConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -20,5 +21,6 @@ class WebSecurityConfig(private val jwtAuthConverter: JwtAuthConverter) {
           .authorizeHttpRequests {
               it.requestMatchers("/**").hasAuthority("access")
           }
+          .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
           .build()
 }
