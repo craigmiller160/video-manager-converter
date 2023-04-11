38b25b81aaf445df052fa7ef6ca3b034a276697a
Craig Miller <craigmiller160@gmail.com>
2023-04-11 12:08:21 -0400

    Got app to start

--- src/main/kotlin/io/craigmiller160/videomanagerconverter/VideoManagerConverterApplication.kt
+++ src/main/kotlin/io/craigmiller160/videomanagerconverter/VideoManagerConverterApplication.kt
@@ -1,9 +1,11 @@
 package io.craigmiller160.videomanagerconverter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = ["io.craigmiller160.videomanagerconverter"])
 class VideoManagerConverterApplication
 
 fun main(args: Array<String>) {
